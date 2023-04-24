d913cf9e029e1c5a4015594e03a348cdbcf8980b
Andrius Makutas <andrius.makutas@gmail.com>
2023-04-24 21:51:52 +0300

    added admin panel additional functionality

--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Author)
-admin.site.register(Book)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'description', 'display_genre')
+    search_fields = ('title', )
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    # def available_on(self, obj):
+    #     return obj.due_back.strftime('%Y-%m-%d')
+
+    list_display = ('book', 'book_status', 'due_back')
+
+    list_filter = ('book_status', 'due_back')
+    fieldsets = (
+        ('General', {'fields': ('instance_id', 'book')}),
+        ('Availability', {'fields': ('book_status', 'due_back')}),
+    )
+    search_fields = ('instance_id', 'book__title')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
--- library/models.py
+++ library/models.py
@@ -9,6 +9,10 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
@@ -21,10 +25,13 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} - {self.last_name}"
 
+    def display_books(self):
+        return ', '.join(book.title for book in self.books.all())
+
 
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='books')
     title = models.CharField('Title', max_length=200, help_text="Enter book title: ")
     description = models.TextField('Description', max_length=1000, help_text="Enter short book description: ")
     isbn = models.CharField(
@@ -36,11 +43,14 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all())
+
 
 class BookInstance(models.Model):
     instance_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique book UUID code")
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
-    due_back = models.DateField("Available", null=True, blank=True)
+    due_back = models.DateField("Available on:", null=True, blank=True)
 
     LOAN_STATUS = (
         ('p', 'Processing'),
@@ -56,4 +66,5 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.book.title}"
+
 
