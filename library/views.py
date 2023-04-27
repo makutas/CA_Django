2e859c43163daa3ea973fd67d090bbede88bc0f3
Paulius Skaisgiris <paulius.skaisgiris@gmail.com>
2023-04-27 21:02:23 +0200

    feat: add list and detailed author and book views

--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Genre, Author, Book, BookInstance
+from django.views import generic
 
 
 def index(request):
@@ -17,3 +18,24 @@ def index(request):
     }
 
     return render(request, "index.html", context)
+
+
+def authors(request):
+    authors = Author.objects.all()
+    return render(request, 'authors.html', {'authors': authors})
+
+
+def author(request, author_id):
+    single_author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'author.html', {'author': single_author})
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = "book_list.html"
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
