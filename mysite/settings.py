cbff01b29ffcf344ac3c367327b9e816bcf7bf3d
Paulius Skaisgiris <paulius.skaisgiris@gmail.com>
2023-05-03 20:52:24 +0200

    feat: add book covers and photo upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,3 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media folder settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'library/static/media')
