e3ad3ffba0def0de059bb1d8f0d97c1df502690f
Paulius Skaisgiris <paulius.skaisgiris@gmail.com>
2023-05-03 20:50:38 +0200

    feat: add pagination to book list and authors

--- library/views.py
+++ library/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Genre, Author, Book, BookInstance
 from django.views import generic
+from django.core.paginator import Paginator
+from django.db.models import Q
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    template_name = "book_list.html"
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
 
 
 def index(request):
@@ -21,8 +34,10 @@ def index(request):
 
 
 def authors(request):
-    authors = Author.objects.all()
-    return render(request, 'authors.html', {'authors': authors})
+    paginator = Paginator(Author.objects.all(), 2)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
+    return render(request, 'authors.html', context={'authors': paged_authors})
 
 
 def author(request, author_id):
@@ -30,12 +45,10 @@ def author(request, author_id):
     return render(request, 'author.html', {'author': single_author})
 
 
-class BookListView(generic.ListView):
-    model = Book
-    template_name = "book_list.html"
-
-
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'book_detail.html'
-
+def search(request):
+    # Ima informaciją iš paieškos laukelio
+    query = request.GET.get('query')
+    # Prafiltruoja pagal įvestą tekstą knygų pavadinimus ir aprašymus.
+    # Icontains nuo contains skiriasi tuo, kad icontains ignoruoja ar raidės didžiosios/mažosios.
+    search_results = Book.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    return render(request, 'search.html', {'books': search_results, 'query': query})
