8b9866265ac16024b795df04d90278e8d419a0cb
Paulius Skaisgiris <paulius.skaisgiris@gmail.com>
2023-05-11 19:46:25 +0200

    feat: add book review input field

--- library/views.py
+++ library/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Author, Book, BookInstance
 from django.views import generic
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
 from django.contrib.auth.forms import User
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
+from .forms import BookReviewForm
+from django.views.generic.edit import FormMixin
 
 
 class BookListView(generic.ListView):
@@ -17,9 +18,27 @@ class BookListView(generic.ListView):
     template_name = "book_list.html"
 
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(FormMixin, generic.DetailView):
     model = Book
     template_name = 'book_detail.html'
+    form_class = BookReviewForm
+
+    def get_success_url(self):
+        return reverse('book-detail', kwargs={'pk': self.object.book_id})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.book = self.object
+        form.instance.reviewer = self.request.user
+        form.save()
+        return super(BookDetailView, self).form_valid(form)
 
 
 def index(request):
